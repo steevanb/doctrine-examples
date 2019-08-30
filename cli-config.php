af7dff34f1e4392446824f84e0a758fbd8d5d6d7
Steevan <steevan.barboyon@gmail.com>
2019-08-30 09:55:02 +0200

    Add Circleci

--- cli-config.php
+++ cli-config.php
@@ -2,7 +2,10 @@
 declare(strict_types=1);
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use DoctrineExamples\Tests\EntityManagerFactory;
 
-require_once __DIR__ . '/tests/bootstrap.php';
+require_once __DIR__ . '/bootstrap.php';
 
-return ConsoleRunner::createHelperSet($GLOBALS['entityManager']);
+return ConsoleRunner::createHelperSet(
+    EntityManagerFactory::getSingleton()
+);
